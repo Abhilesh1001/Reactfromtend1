df026ef715d120241e473cff77a756e539387753
Abhilesh1001 <104245983+Abhilesh1001@users.noreply.github.com>
2023-08-12 19:45:24 +0530

    thirdcommit

--- myproject/src/feature/login/Login.jsx
+++ myproject/src/feature/login/Login.jsx
@@ -89,7 +89,7 @@ const Login = () => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="login">Sign Up</h5>
+                            <h5 className="modal-title" id="login">Login</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <form action="" onSubmit={handleSubmit}>
